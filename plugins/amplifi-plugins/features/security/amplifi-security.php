0b4d6993282132ad3b39347dab27a2d774697b3c
Adam Chiaravalle <adamchiaravalle@gmail.com>
2026-05-28 11:42:07 -0400

    Bump version to 3.0.9

--- plugins/amplifi-plugins/features/security/amplifi-security.php
+++ plugins/amplifi-plugins/features/security/amplifi-security.php
@@ -12,7 +12,7 @@ if ( defined( 'AMPLIFI_SECURITY_VERSION' ) ) {
 	return;
 }
 
-define( 'AMPLIFI_SECURITY_VERSION', '3.0.8' );
+define( 'AMPLIFI_SECURITY_VERSION', '3.0.9' );
 define( 'AMPLIFI_SECURITY_DB_VERSION', '1' );
 define( 'AMPLIFI_SECURITY_FILE', __FILE__ );
 define( 'AMPLIFI_SECURITY_PATH', plugin_dir_path( __FILE__ ) );
