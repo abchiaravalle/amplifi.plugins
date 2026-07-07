1305e2427d235e54496ef99e093582584d93d566
Adam Chiaravalle <adamchiaravalle@Adams-Mac-mini.local>
2026-07-07 17:53:08 -0400

    Bump version to 3.1.6

--- plugins/amplifi-plugins/features/cache/ac-static-cache.php
+++ plugins/amplifi-plugins/features/cache/ac-static-cache.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( defined( 'ACSC_VERSION' ) ) {
 	return;
 }
-define( 'ACSC_VERSION', '3.1.5' );
+define( 'ACSC_VERSION', '3.1.6' );
 define( 'ACSC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ACSC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'ACSC_PLUGIN_FILE', __FILE__ );
